2fa74fb66139c436a64c2e67b8967574bbbd8b1c
QuellaQueue <teddymmartin@gmail.com>
2026-02-18 11:12:39 -0600

    Version 1 - Not in parallel

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # AutoScanLocalIPs
+
+The attached script is designed to gather ip information about the current computer's network adapters, and use it to find other active devices on the same local network.
+
+Steps are broken down as follows
+1. Collect running network adapters.
+2. Get the current network adapter's IP address and Prefix length.
+3. Calculate the prefix - (WIP as this is currently only set to accept the archaic Class A,B,C prefixs /8, /16 and /24 respectively.)
+4. Use the known prefix to check all usable ips under the that prefix
+5. Save responding ips to a .txt file in the same folder
+
+
+
+Notes:
+-Tried swapping this to run in parallel, but due to parallelization requiring far too much of an overhead for a tiny improvement to run-time, I will be reverting the script's loops back to sequential instead. Pretty sure I could force each ping and write to the file to run in asyncronous jobs, but will look into this after swapping back to sequential to get rid of the code clutter.
--- ScanIPsOnNetwork.ps1
+++ ScanIPsOnNetwork.ps1
@@ -1,55 +1,40 @@
+#Checks for active network connections
 $activeadapters = get-netadapter | Where-Object {$_.status -eq "Up"}
 $possibleips = 1..255
+
 #Now we wipe our save file
-$savelocation = "C:\Users\qmmartin\OneDrive - Duly Health and Care\Scripts\Illegals\nearbyips.txt"
+$savelocation = Join-Path -Path $PSScriptRoot -ChildPath "nearbyips.txt"
 "" > $savelocation
 
-#Checks for active network connections
+#Main Loop
 foreach($adapter in $activeadapters){
     Write-Debug ("Adapter online: "+ $adapter.name)
     $nicinfo = Get-NetIPAddress -interfacealias $adapter.name -addressfamily "IPv4"
     $myip = ($nicinfo).IPAddress
 
-    #This is the Subnetmask under a different name. By default this is in Whack notation
+    #This is the Subnetmask's binary length under a different name. By default this is in Whack notation (/8)
     $subnetmask = ($nicinfo).PrefixLength
+
+    #Splits up the host device's IPv4 address, and assigns each octet to a variable
     $ipwhack8 = $myip.split(".")[0]
     $ipwhack16 = $myip.split(".")[1]
     $ipwhack24 = $myip.split(".")[2]
 
-    #
-
-                $ipw8 = $ipwhack8
-                $ipw16 = $ipwhack16
-                $ipw24 = $ipwhack24
-    
-    $networkid
+    #$networkid is unused as of now. May decide to use in the future
     switch ($subnetmask){
         8{$networkid = $ipwhack8 + ".0.0.0"}
-        16{$networkid = $ipwhack8 + $ipwhack16 + ".0.0"}
-        24{$networkid = $ipwhack8 + $ipwhack16 + $ipwhack24 +".0"}
+        16{$networkid = $ipwhack8 + "."+ $ipwhack16 + ".0.0"}
+        24{$networkid = $ipwhack8  +"."+ $ipwhack16 + "." +$ipwhack24 +".0"}
         default{Write-Output "Subnetmask $subnetmask is not a default whack value (8,16,24). Cannot Scan for IPs." }
     }
-    
 
-    #checks all possible IPs and records any that 
+    #Checks all possible IPs on the subnet and records any that respond
     switch($subnetmask){
-        8{    
-            $possibleips | foreach-object -parallel{
-                #pulls in the global variable
-                $ipw8 = $using:ipwhack8
-                
-                $ip1 = $_
-                $possibleips | foreach-object -parallel{
-                    #pulls in the global variable
-                    $ipw8 = $using:ipwhack8
-
-                    $ip2 = $_
-                    $possibleips | foreach-object -parallel{
-                        $ip3 = $_
-                        #pulls in the global variable
-                        $ipw8 = $using:ipwhack8
-
-                        $targetaddress = $ipw8 + "." + $ip1 + "." + $ip2 + "." + $ip3
+        8{   
+            foreach($ip1 in $possibleips){ 
+                foreach($ip2 in $possibleips){
+                    foreach($ip3 in $possibleips){  
+                        $targetaddress = $ipwhack8 + "." + $ip1 + "." + $ip2 + "." + $ip3
                         $result = (Test-Connection -count 1 -targetname $targetaddress).Status
                         if ($result -eq "Success"){
                             Write-output "Success on $targetaddress"
@@ -60,21 +45,9 @@ foreach($adapter in $activeadapters){
             }
         }
         16{
-            $possibleips | foreach-object -parallel{
-                #Pulls in the global variables
-                $ipw8 = $using:ipwhack8
-                $ipw16 = $using:ipwhack16
-
-                #non-volatiles the counter 
-                $ip2 = $_
-                $possibleips | foreach-object -parallel{
-                    #Pulls in the global variables
-                    $ipw8 = $using:ipwhack8
-                    $ipw16 = $using:ipwhack16
-
-                    #Non-volitiles the counter
-                    $ip3 = $_
-                    $targetaddress = $ipw8 + "." + $ipw16 + "." + $ip2 + "." + $ip3
+            foreach($ip2 in $possibleips){
+                foreach($ip3 in $possibleips){
+                    $targetaddress = $ipwhack8 + "." + $ipwhack16 + "." + $ip2 + "." + $ip3
                     $result = (Test-Connection -count 1 -targetname $targetaddress).Status
                     if ($result -eq "Success"){
                         Write-output "Success on $targetaddress"
@@ -86,10 +59,7 @@ foreach($adapter in $activeadapters){
         24{
             foreach($ip in $possibleips){
                 #pulls in the global variables
-                $ipw8 = $ipwhack8
-                $ipw16 = $ipwhack16
-                $ipw24 = $ipwhack24
-                $targetaddress = $ipw8 + "." + $ipw16 + "." + $ipw24 + "." + $ip
+                $targetaddress = $ipwhack8 + "." + $ipwhack16 + "." + $ipwhack24 + "." + $ip
                 $result = (Test-Connection -count 1 -targetname ($targetaddress)).Status 
                 if ($result -eq "Success"){
                     Write-output "Success on $targetaddress"
